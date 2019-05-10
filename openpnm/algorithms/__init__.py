11396e8d1e030ba9d0988ce323683e56baa22f77
Nil2M <misaghian.aut@gmail.com>
2019-05-10 11:02:50 -0400

    [ci skip]

--- openpnm/algorithms/__init__.py
+++ openpnm/algorithms/__init__.py
@@ -28,3 +28,5 @@ from .MixedInvasionPercolation import MixedInvasionPercolation
 from .Porosimetry import Porosimetry
 from .NernstPlanck import NernstPlanck
 from .ChargeConservationNernstPlanck import ChargeConservationNernstPlanck
+from .DirectionalRelativePermeability import DirectionalRelativePermeability
+from .RelativePermeability import RelativePermeability
